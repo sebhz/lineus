739e1582b6f5ebab3afbfedfe0b39eb389a3e807
sebhz <seb@zyglute.fr>
2021-03-25 21:34:13 +0100

    Fix fitting logic
    Should now work whatever the orientations of
    all the axis. Invert the Y axis, since PIL
    has Y increasing down.

--- roulette.py
+++ roulette.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' Simple roulette computation and visualization '''
 import argparse
-from math import pi, cos, sin
+from math import pi, cos, sin, copysign
 from PIL import Image, ImageDraw
 
 class Cycloidal():
@@ -55,7 +55,8 @@ def get_fit_function(from_box, to_box):
     ''' Return a function transforming coordinates to center
         a figure contained in from_box, when projecting it
         inside to_box, so that it takes as much space as possible
-        while keeping its aspect ratio '''
+        while keeping its aspect ratio.
+        Also, invert the x and y axis if needed '''
     (x0_from, y0_from, x1_from, y1_from) = from_box
     (x0_to, y0_to, x1_to, y1_to) = to_box
     (w_from, h_from) = (x1_from-x0_from, y1_from-y0_from)
@@ -63,13 +64,15 @@ def get_fit_function(from_box, to_box):
     (a_from, a_to) = (abs(w_from/h_from), abs(w_to/h_to))
 
     if a_from > a_to: # Landscape to portrait
-        scale = w_to/w_from
-        trans = (x0_to, y0_to + (h_to - scale*h_from)/2)
+        xscale = w_to/w_from
+        yscale = copysign(xscale, h_to*h_from)
+        trans = (x0_to, y0_to + (h_to - h_from*yscale)/2)
     else: # Portrait to landscape
-        scale = h_to/h_from
-        trans = (x0_to + (w_to - scale*w_from)/2, y0_to)
+        yscale = h_to/h_from
+        xscale = copysign(yscale, w_to*w_from)
+        trans = (x0_to + (w_to - w_from*xscale)/2, y0_to)
 
-    return lambda p: (trans[0] + scale*(p[0]-x0_from), trans[1] + scale*(p[1]-y0_from))
+    return lambda p: (trans[0] + xscale*(p[0]-x0_from), trans[1] + yscale*(p[1]-y0_from))
 
 def draw_cycloidal(points, pixel_size):
     ''' Draw our nice cycloid '''
@@ -79,14 +82,14 @@ def draw_cycloidal(points, pixel_size):
     (maxx, maxy) = map(max, zip(*points))
     (minx, miny) = map(min, zip(*points))
 
-    fit_func = get_fit_function((minx, miny, maxx, maxy), (0, 0, pixel_size, pixel_size))
+    fit_func = get_fit_function((minx, miny, maxx, maxy), (0, pixel_size, pixel_size, 0))
 
     p0 = fit_func(points[0])
     porig = p0
     for p1 in points[1:]:
-        draw.line(p0 + fit_func(p1), fill=(0, 0, 0), width=2)
+        draw.line(p0 + fit_func(p1), fill=(0, 0, 0))
         p0 = fit_func(p1)
-    draw.line(p0 + porig, fill=(0, 0, 0), width=2)
+    draw.line(p0 + porig, fill=(0, 0, 0))
     im.show()
 
 def parse_args():
